e2eb838379c79fc6f285b6ca11985b7566b3a428
JorritMontijn <jorritmontijn@gmail.com>
2019-04-04 10:55:19 +0200

    Update label2idx.m

--- data_handling/label2idx.m
+++ data_handling/label2idx.m
@@ -1,12 +1,15 @@
-function [varDataOut,vecUnique] = label2idx(varData)
-	%UNTITLED6 Summary of this function goes here
-	%   Detailed explanation goes here
+function [varDataOut,vecUnique,vecCounts] = label2idx(varData)
+	%label2idx Transforms label-entries to consecutive integer-based data
+	%Syntax: [varDataOut,vecUnique,vecCounts] = label2idx(varData)
 	
 	varDataOut = nan(size(varData));
 	vecUnique = unique(varData);
+	vecCounts = zeros(size(vecUnique));
 	vecIdx = 1:length(vecUnique);
 	for intIdx=vecIdx
-		varDataOut(varData==vecUnique(intIdx)) = intIdx;
+		indEntries = varData==vecUnique(intIdx);
+		varDataOut(indEntries) = intIdx;
+		vecCounts(intIdx) = sum(indEntries);
 	end
 end
 
