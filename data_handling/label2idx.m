d3bcc9120129836b700d8fe61d7bfbab58504b7b
JorritMontijn <jorritmontijn@gmail.com>
2019-09-16 12:42:21 +0200

    20190916
    Includes some changes used by EyeTracker

--- data_handling/label2idx.m
+++ data_handling/label2idx.m
@@ -8,12 +8,24 @@ function [varDataOut,vecUnique,vecCounts,cellSelect,vecRepetition] = label2idx(v
 	vecIdx = 1:length(vecUnique);
 	cellSelect = cell(1,length(vecUnique));
 	vecRepetition = zeros(1,length(vecUnique));
-	for intIdx=vecIdx
-		indEntries = varData==vecUnique(intIdx);
-		cellSelect{intIdx} = indEntries;
-		varDataOut(indEntries) = intIdx;
-		vecCounts(intIdx) = sum(indEntries);
-		vecRepetition(indEntries) = 1:sum(indEntries);
+	if iscell(vecUnique)
+		%character array
+		for intIdx=vecIdx
+			indEntries = strcmp(varData,vecUnique{intIdx});
+			cellSelect{intIdx} = indEntries;
+			varDataOut(indEntries) = intIdx;
+			vecCounts(intIdx) = sum(indEntries);
+			vecRepetition(indEntries) = 1:sum(indEntries);
+		end
+	else
+		%numeric vector
+		for intIdx=vecIdx
+			indEntries = varData==vecUnique(intIdx);
+			cellSelect{intIdx} = indEntries;
+			varDataOut(indEntries) = intIdx;
+			vecCounts(intIdx) = sum(indEntries);
+			vecRepetition(indEntries) = 1:sum(indEntries);
+		end
 	end
 end
 
