87f75c353f3dafb08b55bcf525d71637020029ec
JorritMontijn <jorritmontijn@gmail.com>
2020-08-28 14:14:08 +0200

    20200828

--- CheckEsc.m
+++ CheckEsc.m
@@ -1,8 +1,15 @@
 function boolEsc = CheckEsc()
 	%CheckEsc Checks if escape button is pressed
 	%   boolEsc=any(strcmpi(KbName(keyCode),'escape'));
-	KbName('UnifyKeyNames');
-	[keyIsDown, secs, keyCode] = KbCheck();
-	boolEsc=any(strcmpi(KbName(keyCode),'escape'));
+	
+	%check if psychtoolbox is installed
+	if exist('KbName','file')
+		KbName('UnifyKeyNames');
+		[keyIsDown, secs, keyCode] = KbCheck();
+		boolEsc=any(strcmpi(KbName(keyCode),'escape'));
+	else
+		%otherwise use Mario Koddenbrock's way
+		boolEsc = getAsyncKeyState(VirtualKeyCode.VK_ESCAPE);
+	end
 end
 
