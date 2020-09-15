854a6f3735499233cf95ae0a258e08b45d9fdba3
JorritMontijn <jorritmontijn@gmail.com>
2020-09-15 15:20:31 +0200

    Update getR2.m

--- statistics/getR2.m
+++ statistics/getR2.m
@@ -1,4 +1,4 @@
-function [dblR2,dblSS_tot,dblSS_res] = getR2(vecY,vecFitY)
+function [dblR2,dblSS_tot,dblSS_res,dblT,dblP] = getR2(vecY,vecFitY,intK)
 	%getR2 Calculates R-squared
 	%   [dblR2,dblSS_tot,dblSS_res] = getR2(vecY,vecFitY)
 	indUseVals = ~isnan(vecY) & ~isnan(vecFitY);
@@ -10,5 +10,13 @@ function [dblR2,dblSS_tot,dblSS_res] = getR2(vecY,vecFitY)
 	dblSS_tot = sum((vecY - mean(vecY)).^2);
 	dblSS_res = sum((vecY - vecFitY).^2);
 	dblR2 = 1 - (dblSS_res / dblSS_tot);	
+	
+	%p-value
+	dblT = [];
+	dblP = [];
+	if exist('intK','var') && ~isempty(intK)
+		dblT=sqrt((dblR2*(numel(vecY)-intK-1))/(1-dblR2) );
+		dblP = 1-tcdf(dblT,numel(vecY)-1);
+	end
 end
 
