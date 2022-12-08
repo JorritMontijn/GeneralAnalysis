5900696c5690fbb5174f71b63026663958dd87c5
JorritMontijn <jorritmontijn@gmail.com>
2022-12-08 11:11:10 +0100

    Update datemod.m

--- data_handling/datemod.m
+++ data_handling/datemod.m
@@ -1,8 +1,11 @@
 function strDate = datemod(strFile)
 	%datemod Returns last modified date for file
 	%   strDate = datemod(strFile)
-	
-	sDate = System.IO.File.GetLastWriteTime(strFile);
-	strDate = sprintf('%04d-%02d-%02d',sDate.Year,sDate.Month,sDate.Day);
+	try
+		sDate = System.IO.File.GetLastWriteTime(strFile);
+		strDate = sprintf('%04d-%02d-%02d',sDate.Year,sDate.Month,sDate.Day);
+	catch
+		strDate = 'Unable-to-read';
+	end
 end
 
