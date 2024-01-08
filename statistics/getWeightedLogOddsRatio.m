16ad745d44fe1bd280662d2f74317d8cc2c9f764
Jorrit Montijn <jorritmontijn@gmail.com>
2024-01-08 17:50:30 +0100

    Update getWeightedLogOddsRatio.m

--- statistics/getWeightedLogOddsRatio.m
+++ statistics/getWeightedLogOddsRatio.m
@@ -7,10 +7,9 @@ function dblWLOR = getWeightedLogOddsRatio(vecData)
 	end
 	
 	%sort
-	vecData = sort(vecData(:));
-	vecNorm = linspace(1/numel(vecData),1,numel(vecData));
-	vecLogOdds = abs(log(vecData(:)./vecNorm(:)));
-	vecLogStep = diff(log(vecData(:)));
-	dblWLOR = sum(vecLogOdds(1:(end-1)).*vecLogStep)/sum(vecLogStep);
+	vecNorm = linspace(1/numel(vecData),1,numel(vecData)); %theoretical norm, uniform over [0,1]
+	vecLogOdds = abs(log(vecData(:)./vecNorm(:))); %absolute of log-odds of random controls to theoretical norm
+	vecLogStep = diff(log(vecData(:))); %step size of random controls on log scale
+	dblWLOR = sum(vecLogOdds(1:(end-1)).*vecLogStep)/sum(vecLogStep); %sum over absolute log-odds weighted by log step size
 end
 
