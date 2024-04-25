6959a87c6caa113e79efa48807e2eb52db01d313
JorritMontijn <jorritmontijn@gmail.com>
2024-04-25 13:03:47 +0200

    Update getdprime2.m

--- statistics/getdprime2.m
+++ statistics/getdprime2.m
@@ -1,3 +1,6 @@
-function dPrime = getdprime2(vecIn1,vecIn2)
-	dPrime = (mean(vecIn1) - mean(vecIn2))/sqrt(0.5*(var(vecIn1)+var(vecIn2)));
+function dPrime = getdprime2(vecIn1,vecIn2,intDim)
+	if ~exist('intDim','var') || isempty(intDim)
+		intDim=1;
+	end
+	dPrime = (mean(vecIn1,intDim) - mean(vecIn2,intDim))./sqrt(0.5*(var(vecIn1,[],intDim)+var(vecIn2,[],intDim)));
 end
